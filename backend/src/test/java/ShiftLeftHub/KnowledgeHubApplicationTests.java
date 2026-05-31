b8f174e7e0dc158148da47be47707d4b890725b3
HomieEddy <mceddy95@hotmail.fr>
2026-05-31 10:33:12 -0400

    feat: initialize backend with Maven setup and Spring Boot application

---
+++ backend/src/test/java/ShiftLeftHub/KnowledgeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ShiftLeftHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
