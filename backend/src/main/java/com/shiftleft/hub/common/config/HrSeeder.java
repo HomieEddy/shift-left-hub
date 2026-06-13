e70e073d11c6c71cea2a507befd88137fbb170f0
HomieEddy <mceddy95@hotmail.fr>
2026-06-13 00:05:57 -0400

    fix(14-seeding-revamp): remove unused imports from HrSeeder and ItSeeder (IN-01)

--- backend/src/main/java/com/shiftleft/hub/common/config/HrSeeder.java
+++ backend/src/main/java/com/shiftleft/hub/common/config/HrSeeder.java
@@ -26,8 +26,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
